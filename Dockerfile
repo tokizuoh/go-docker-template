8edd8ed5cce237c480ad87fa6a2950a7350cc6c5
tokizuoh <tokizo819@gmail.com>
2021-08-26 08:45:04 +0900

    add processing go mod init by .sh

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,9 @@ FROM golang:1.16-alpine
 WORKDIR /go/src
 
 COPY ./ ./
+# TODO[#1]: 依存関係が無い時にgo mod downloadしても回避できるようにする
 # RUN go mod download
 RUN apk add --no-cache gcc musl-dev
+
+# Generate go.mod
+RUN sh ./setup/go_mod_init.sh
---
+++ setup/go_mod_init.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+source ./setup/.env
+
+readonly GITHUB_ID="tokizuoh"
+readonly MOD_FILE="go.mod"
+
+if [ -e $MOD_FILE ]; then
+    printf "%s already exists.\n" $MOD_FILE
+    exit 0
+fi
+
+read module
+command=$(printf "go mod init github.com/%s/%s" $GITHUB_ID $MODULE)
+eval $command
+
+printf "Finished!: go mod init github.com/%s/%s \n" $GITHUB_ID $MODULE
